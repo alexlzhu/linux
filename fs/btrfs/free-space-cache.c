29f32154da0fb3f4c94f9fc3807ecfb06f004c9e
Josef Bacik <jbacik@fb.com>
2020-06-10 07:18:20 -0700

    btrfs: block group leak fixes
    Summary:
    btrfs: block group leak fixes
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- fs/btrfs/free-space-cache.c
+++ fs/btrfs/free-space-cache.c
@@ -2845,8 +2845,10 @@ __btrfs_return_cluster_to_free_space(
 	struct rb_node *node;
 
 	spin_lock(&cluster->lock);
-	if (cluster->block_group != block_group)
-		goto out;
+	if (cluster->block_group != block_group) {
+		spin_unlock(&cluster->lock);
+		return 0;
+	}
 
 	cluster->block_group = NULL;
 	cluster->window_start = 0;
@@ -2887,7 +2889,6 @@ __btrfs_return_cluster_to_free_space(
 	}
 	cluster->root = RB_ROOT;
 
-out:
 	spin_unlock(&cluster->lock);
 	btrfs_put_block_group(block_group);
 	return 0;
