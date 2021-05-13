59c1ebb120e92138f6ec08a4d569c134193cf26e
Josef Bacik <jbacik@fb.com>
2021-05-13 08:51:36 -0700

    FB-ONLY: make the unlink fsnotify events happen after d_delete
    Summary:
    FB-ONLY: make the unlink fsnotify events happen after d_delete
    
    Watchman reported a problem where their monitoring tool would see a file
    unlink, but would immediately stat that file and see that it still
    existed.  This was caused by the fsnotify people moving the notification
    to right after the unlink, meaning there's a gap in time where the file
    is unlinked but the dentry still exists.
    
    This will be fixed upstream by a rather large rework, but the actual fix
    is simple enough.  Simply remove the previous notifications and bundle
    it with a d_delete helper that will hold onto the dentry->d_inode until
    after we've emitted the notification.  This fixes the reproducer that I
    wrote.
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    Signed-off-by: Josef Bacik <jbacik@fb.com>

--- fs/namei.c
+++ fs/namei.c
@@ -3963,13 +3963,12 @@ int vfs_rmdir(struct inode *dir, struct dentry *dentry)
 	dentry->d_inode->i_flags |= S_DEAD;
 	dont_mount(dentry);
 	detach_mounts(dentry);
-	fsnotify_rmdir(dir, dentry);
 
 out:
 	inode_unlock(dentry->d_inode);
 	dput(dentry);
 	if (!error)
-		d_delete(dentry);
+		d_delete_notify(dir, dentry);
 	return error;
 }
 EXPORT_SYMBOL(vfs_rmdir);
@@ -4080,7 +4079,6 @@ int vfs_unlink(struct inode *dir, struct dentry *dentry, struct inode **delegate
 			if (!error) {
 				dont_mount(dentry);
 				detach_mounts(dentry);
-				fsnotify_unlink(dir, dentry);
 			}
 		}
 	}
@@ -4090,7 +4088,7 @@ out:
 	/* We don't d_delete() NFS sillyrenamed files--they still exist. */
 	if (!error && !(dentry->d_flags & DCACHE_NFSFS_RENAMED)) {
 		fsnotify_link_count(target);
-		d_delete(dentry);
+		d_delete_notify(dir, dentry);
 	}
 
 	return error;
