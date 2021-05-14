2056fe6d99dc72287f0713930d6a0579ad3f0efe
Jakub Kicinski <kuba@kernel.org>
2021-05-14 13:04:25 -0700

    [5.6] tcp: add tracepoint for checksum errors
    Summary:
        tcp: add tracepoint for checksum errors
    
        Add a tracepoint for capturing TCP segments with
        a bad checksum. This makes it easy to identify
        sources of bad frames in the fleet (e.g. machines
        with faulty NICs).
    
        It should also help tools like IOvisor's tcpdrop.py
        which are used today to get detailed information
        about such packets.
    
        We don't have a socket in many cases so we must
        open code the address extraction based just on
        the skb.
    
        v2: add missing export for ipv6=m
    
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
        Signed-off-by: David S. Miller <davem@davemloft.net>
        (cherry picked from commit 709c0314239992162cba26a860f04319a15860c4)
    
    Test Plan: Not much testing TBH, just basic TCP. Tested extensively the net-next version, the code did not change in recent releases.
    
    Reviewers: yhs
    
    Subscribers: ntspring
    
    Tasks: T90335615
    
    Tags: 
    
    Blame Revision: 
    
    
    Differential Revision: https://phabricator.intern.facebook.com/D28515917

--- net/ipv6/tcp_ipv6.c
+++ net/ipv6/tcp_ipv6.c
@@ -1487,6 +1487,7 @@ discard:
 	kfree_skb(skb);
 	return 0;
 csum_err:
+	trace_tcp_bad_csum(skb);
 	TCP_INC_STATS(sock_net(sk), TCP_MIB_CSUMERRORS);
 	TCP_INC_STATS(sock_net(sk), TCP_MIB_INERRS);
 	goto discard;
@@ -1703,6 +1704,7 @@ no_tcp_socket:
 
 	if (tcp_checksum_complete(skb)) {
 csum_error:
+		trace_tcp_bad_csum(skb);
 		__TCP_INC_STATS(net, TCP_MIB_CSUMERRORS);
 bad_packet:
 		__TCP_INC_STATS(net, TCP_MIB_INERRS);
