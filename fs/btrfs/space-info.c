b8fab5bd4b78391e8dd75cd3334f72929b5bd0a1
Josef Bacik <jbacik@fb.com>
2021-06-10 07:14:47 -0700

    btrfs: don't trim nr_pages if we want to flush everything
    Summary:
    btrfs: don't trim nr_pages if we want to flush everything
    
    This technically shouldn't matter because we're accounting for all
    delalloc pages in the system, but with compression it will go to 0
    before we've actually written anything, so we need to make sure we're
    setting it to LLONG_MAX.
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    Signed-off-by: Josef Bacik <jbacik@fb.com>
    Signed-off-by: Chris Mason <clm@fb.com>

--- fs/btrfs/space-info.c
+++ fs/btrfs/space-info.c
@@ -537,8 +537,14 @@ static void shrink_delalloc(struct btrfs_fs_info *fs_info,
 
 	loops = 0;
 	while ((delalloc_bytes || ordered_bytes) && loops < 3) {
-		u64 temp = min(delalloc_bytes, to_reclaim) >> PAGE_SHIFT;
-		long nr_pages = min_t(u64, temp, LONG_MAX);
+		long nr_pages;
+
+		if (to_reclaim == U64_MAX) {
+			nr_pages = LONG_MAX;;
+		} else {
+			u64 temp = min(delalloc_bytes, to_reclaim) >> PAGE_SHIFT;
+			nr_pages = min_t(u64, temp, LONG_MAX);
+		}
 
 		btrfs_start_delalloc_roots(fs_info, nr_pages, true);
 
