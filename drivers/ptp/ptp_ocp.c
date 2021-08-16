ad99bf8374c43e2eb6ac13de36983063ecfe191a
Jonathan Lemon <jonathan.lemon@gmail.com>
2021-08-16 15:13:35 -0700

    ptp: ocp: Fix error path for pci_ocp_device_init()
    If ptp_ocp_device_init() fails, pci_disable_device() is skipped.
    Fix the error handling so this case is covered.  Update ptp_ocp_remove()
    so the normal exit path is identical.
    
    Reported-by: Hulk Robot <hulkci@huawei.com>
    Fixes: 773bda964921 ("ptp: ocp: Expose various resources on the timecard.")
    Signed-off-by: Jonathan Lemon <jonathan.lemon@gmail.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    (cherry picked from commit d9fdbf132dab4d844167004ae4ca979fbebd9871)
    Signed-off-by: Jonathan Lemon <jonathan.lemon@gmail.com>

--- drivers/ptp/ptp_ocp.c
+++ drivers/ptp/ptp_ocp.c
@@ -1456,7 +1456,7 @@ ptp_ocp_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 	bp = devlink_priv(devlink);
 	err = ptp_ocp_device_init(bp, pdev);
 	if (err)
-		goto out_unregister;
+		goto out_disable;
 
 	/* compat mode.
 	 * Older FPGA firmware only returns 2 irq's.
@@ -1494,8 +1494,9 @@ ptp_ocp_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 
 out:
 	ptp_ocp_detach(bp);
-	pci_disable_device(pdev);
 	pci_set_drvdata(pdev, NULL);
+out_disable:
+	pci_disable_device(pdev);
 out_unregister:
 	ptp_ocp_devlink_unregister(devlink);
 out_free:
@@ -1511,8 +1512,8 @@ ptp_ocp_remove(struct pci_dev *pdev)
 	struct devlink *devlink = priv_to_devlink(bp);
 
 	ptp_ocp_detach(bp);
-	pci_disable_device(pdev);
 	pci_set_drvdata(pdev, NULL);
+	pci_disable_device(pdev);
 
 	ptp_ocp_devlink_unregister(devlink);
 	devlink_free(devlink);
