fe69cd2ed4591c8b7b09ea521a46f60afd2c8ebe
Masami Hiramatsu <mhiramat@kernel.org>
2021-09-14 23:40:07 +0900

    kprobes: treewide: Use 'kprobe_opcode_t *' for the code address in get_optimized_kprobe()
    Since get_optimized_kprobe() is only used inside kprobes,
    it doesn't need to use 'unsigned long' type for 'addr' parameter.
    Make it use 'kprobe_opcode_t *' for the 'addr' parameter and
    subsequent call of arch_within_optimized_kprobe() also should use
    'kprobe_opcode_t *'.
    
    Note that MAX_OPTIMIZED_LENGTH and RELATIVEJUMP_SIZE are defined
    by byte-size, but the size of 'kprobe_opcode_t' depends on the
    architecture. Therefore, we must be careful when calculating
    addresses using those macros.
    
    Link: https://lkml.kernel.org/r/163163040680.489837.12133032364499833736.stgit@devnote2
    
    Signed-off-by: Masami Hiramatsu <mhiramat@kernel.org>
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>
    (cherry picked from commit c42421e205fc2570a4d019184ea7d6c382c93f4c)
    
     # Conflicts:
     #	arch/powerpc/kernel/optprobes.c

--- arch/powerpc/kernel/optprobes.c
+++ arch/powerpc/kernel/optprobes.c
@@ -337,9 +337,8 @@ void arch_unoptimize_kprobes(struct list_head *oplist,
 	}
 }
 
-int arch_within_optimized_kprobe(struct optimized_kprobe *op,
-				 unsigned long addr)
+int arch_within_optimized_kprobe(struct optimized_kprobe *op, kprobe_opcode_t *addr)
 {
-	return ((unsigned long)op->kp.addr <= addr &&
-		(unsigned long)op->kp.addr + RELATIVEJUMP_SIZE > addr);
+	return (op->kp.addr <= addr &&
+		op->kp.addr + (RELATIVEJUMP_SIZE / sizeof(kprobe_opcode_t)) > addr);
 }
