176d27af9db089beefeb53e12623cbc287980ba7
Song Liu <songliubraving@fb.com>
2021-09-29 12:43:13 -0700

    perf/core: fix userpage->time_enabled of inactive events
    Summary:
    perf/core: fix userpage->time_enabled of inactive events
    
    Users of rdpmc rely on the mmapped user page to calculate accurate
    time_enabled. Currently, userpage->time_enabled is only updated when the
    event is added to the pmu. As a result, inactive event (due to counter
    multiplexing) does not have accurate userpage->time_enabled. This can
    be reproduced with something like:
    
    /* open 20 task perf_event "cycles", to create multiplexing */
    
    fd = perf_event_open();  /* open task perf_event "cycles" */
    userpage = mmap(fd);     /* use mmap and rdmpc */
    
    while (true) {
    time_enabled_mmap = xxx; /* use logic in perf_event_mmap_page */
    time_enabled_read = read(fd).time_enabled;
    if (time_enabled_mmap > time_enabled_read)
    BUG();
    }
    
    Fix this by updating userpage for inactive events in merge_sched_in.
    
    
    (cherry picked from commit f792565326825ed806626da50c6f9a928f1079c1)
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    Suggested-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reported-and-tested-by: Lucian Grijincu <lucian@fb.com>
    Signed-off-by: Song Liu <songliubraving@fb.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20210929194313.2398474-1-songliubraving@fb.com

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -3435,6 +3435,29 @@ static int visit_groups_merge(struct perf_event_groups *groups, int cpu,
 	return 0;
 }
 
+static inline bool event_update_userpage(struct perf_event *event)
+{
+	if (likely(!atomic_read(&event->mmap_count)))
+		return false;
+
+	perf_event_update_time(event);
+	perf_set_shadow_time(event, event->ctx);
+	perf_event_update_userpage(event);
+
+	return true;
+}
+
+static inline void group_update_userpage(struct perf_event *group_event)
+{
+	struct perf_event *event;
+
+	if (!event_update_userpage(group_event))
+		return;
+
+	for_each_sibling_event(event, group_event)
+		event_update_userpage(event);
+}
+
 static int merge_sched_in(struct perf_event *event, void *data)
 {
 	struct perf_event_context *ctx = event->ctx;
@@ -3453,13 +3476,14 @@ static int merge_sched_in(struct perf_event *event, void *data)
 	}
 
 	if (event->state == PERF_EVENT_STATE_INACTIVE) {
+		*can_add_hw = 0;
 		if (event->attr.pinned) {
 			perf_cgroup_event_disable(event, ctx);
 			perf_event_set_state(event, PERF_EVENT_STATE_ERROR);
+		} else {
+			ctx->rotate_necessary = 1;
+			group_update_userpage(event);
 		}
-
-		*can_add_hw = 0;
-		ctx->rotate_necessary = 1;
 	}
 
 	return 0;
@@ -5951,6 +5975,8 @@ accounting:
 
 		ring_buffer_attach(event, rb);
 
+		perf_event_update_time(event);
+		perf_set_shadow_time(event, event->ctx);
 		perf_event_init_userpage(event);
 		perf_event_update_userpage(event);
 	} else {
