d2d6516c0ccc6a26d8c2509ac5731e98eab56943
Jakub Kicinski <kuba@kernel.org>
2022-01-20 14:11:19 -0800

    ipv6: gro: flush instead of assuming different flows on hop_limit mismat
    Summary:
        ipv6: gro: flush instead of assuming different flows on hop_limit mismatch
    
        IPv6 GRO considers packets to belong to different flows when their
        hop_limit is different. This seems counter-intuitive, the flow is
        the same. hop_limit may vary because of various bugs or hacks but
        that doesn't mean it's okay for GRO to reorder packets.
    
        Practical impact of this problem on overall TCP performance
        is unclear, but TCP itself detects this reordering and bumps
        TCPSACKReorder resulting in user complaints.
    
        Eric warns that there may be performance regressions in setups
        which do packet spraying across similar RTT but different hop
        count. Existence of such spraying has not been confirmed, and
        we are still not sensitive to varying the flow label, even with
        this change applied. To be safe, however, let's not consider
        this a fix / backport material.
    
        Note that the code plays an easy to miss trick by upcasting next_hdr
        to a u16 pointer and compares next_hdr and hop_limit in one go.
    
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
        Signed-off-by: David S. Miller <davem@davemloft.net>
        (cherry picked from commit 6fc2f3832d36d74289680a1bf4484def8321f6e0)
    
    Test Plan: lots of kperf runs
    
    Reviewers: kafai, ast, bsd
    
    Subscribers: 
    
    Tasks: T109423888
    
    Tags: 
    
    
    Differential Revision: https://phabricator.intern.facebook.com/D33770865

--- net/ipv6/ip6_offload.c
+++ net/ipv6/ip6_offload.c
@@ -248,9 +248,9 @@ INDIRECT_CALLABLE_SCOPE struct sk_buff *ipv6_gro_receive(struct list_head *head,
 		 * memcmp() alone below is sufficient, right?
 		 */
 		 if ((first_word & htonl(0xF00FFFFF)) ||
-		    !ipv6_addr_equal(&iph->saddr, &iph2->saddr) ||
-		    !ipv6_addr_equal(&iph->daddr, &iph2->daddr) ||
-		    *(u16 *)&iph->nexthdr != *(u16 *)&iph2->nexthdr) {
+		     !ipv6_addr_equal(&iph->saddr, &iph2->saddr) ||
+		     !ipv6_addr_equal(&iph->daddr, &iph2->daddr) ||
+		     iph->nexthdr != iph2->nexthdr) {
 not_same_flow:
 			NAPI_GRO_CB(p)->same_flow = 0;
 			continue;
@@ -261,7 +261,8 @@ not_same_flow:
 				goto not_same_flow;
 		}
 		/* flush if Traffic Class fields are different */
-		NAPI_GRO_CB(p)->flush |= !!(first_word & htonl(0x0FF00000));
+		NAPI_GRO_CB(p)->flush |= !!((first_word & htonl(0x0FF00000)) |
+			(__force __be32)(iph->hop_limit ^ iph2->hop_limit));
 		NAPI_GRO_CB(p)->flush |= flush;
 
 		/* If the previous IP ID value was based on an atomic
