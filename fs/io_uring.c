d3fdfdd2819c30e4fb9d3315d615fd07f139d8a5
Jens Axboe <axboe@kernel.dk>
2022-04-22 06:05:49 -0600

    io_uring: fix freeing of cached bio
    A previous commit was incomplete, and never got synced with the fixed
    version.
    
    Fixes: 984f3694e908 ("block: enable bio allocation cache for IRQ driven IO")
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -2948,14 +2948,9 @@ static bool __io_complete_rw_common(struct io_kiocb *req, long res)
 	return false;
 }
 
-static inline void io_req_task_complete(struct io_kiocb *req, bool *locked)
+static inline void __io_req_task_complete(struct io_kiocb *req, bool *locked)
 {
 	int res = req->result;
-
-#ifdef CONFIG_BLOCK
-	if (req->rw.kiocb.ki_flags & IOCB_PRIV_IS_BIO)
-		bio_put(req->rw.kiocb.private);
-#endif
 
 	if (*locked) {
 		io_req_complete_state(req, res, io_put_kbuf(req, 0));
@@ -2964,6 +2959,17 @@ static inline void io_req_task_complete(struct io_kiocb *req, bool *locked)
 		io_req_complete_post(req, res,
 					io_put_kbuf(req, IO_URING_F_UNLOCKED));
 	}
+}
+
+
+static inline void io_req_task_complete(struct io_kiocb *req, bool *locked)
+{
+#ifdef CONFIG_BLOCK
+	if (req->rw.kiocb.ki_flags & IOCB_PRIV_IS_BIO)
+		bio_put(req->rw.kiocb.private);
+#endif
+
+	__io_req_task_complete(req, locked);
 }
 
 static void __io_complete_rw(struct io_kiocb *req, long res,
@@ -6367,7 +6373,7 @@ static int io_poll_update(struct io_kiocb *req, unsigned int issue_flags)
 	req_set_fail(preq);
 	preq->result = -ECANCELED;
 	locked = !(issue_flags & IO_URING_F_UNLOCKED);
-	io_req_task_complete(preq, &locked);
+	__io_req_task_complete(preq, &locked);
 out:
 	if (ret < 0)
 		req_set_fail(req);
@@ -6394,7 +6400,7 @@ static enum hrtimer_restart io_timeout_fn(struct hrtimer *timer)
 		req_set_fail(req);
 
 	req->result = -ETIME;
-	req->io_task_work.func = io_req_task_complete;
+	req->io_task_work.func = __io_req_task_complete;
 	io_req_task_work_add(req, false);
 	return HRTIMER_NORESTART;
 }
